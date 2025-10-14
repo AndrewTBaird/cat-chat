ac3d3bbb0a565c2e9ab875412e9c22ed31062a44
Andrew Baird <andrew@minthome.com>
2025-10-13 21:17:49 -0700

    add vite start command

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   server: {
     port: 5173
   },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    strictPort: false,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
