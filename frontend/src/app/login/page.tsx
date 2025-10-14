bc82eea425c0efc923332d7f7bd9657b80d28ce5
Andrew Baird <andrew@minthome.com>
2025-10-14 11:50:59 -0700

    pull in the login card to the login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,9 +1,15 @@
+import { LoginForm } from "@/components/login-form"
+import { Card } from "@/components/ui/card"
 import React from "react"
 
 const Login = () => {
-  return (<>
-    i'm the login page
-  </>)
+  return (
+    <div className="flex items-center justify-center">
+      <div className="min-w-80">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
 
 export default Login
