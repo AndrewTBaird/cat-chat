bf5bca3321d364a97db4775ecec7c39a7a6f1cd8
Andrew Baird <andrew@minthome.com>
2025-10-13 21:42:37 -0700

    strict prot

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-    strictPort: false,
+    strictPort: true,
   },
   plugins: [react(), tailwindcss()],
   resolve: {
