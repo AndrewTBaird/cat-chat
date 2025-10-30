ab743459fffc6c1d027979e2e244bf53f7dd38c5
Andrew Baird <andrew@minthome.com>
2025-10-30 17:20:53 -0400

    styles!!!

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        serif: ["var(--font-serif)"],
+        mono: ["var(--font-mono)"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
